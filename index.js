17f29c858ef94e6bc94eca41b33b5595b488b6bb
Kelvin Muturi <muturikelvin893@gmail.com>
2023-03-14 01:12:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "weller";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, w!`;
+ 
